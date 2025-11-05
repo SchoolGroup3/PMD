2397179b6b30a98e23cd8f8ed56795be84e936a8
kev12s <kev12s@gmail.com>
2025-11-05 14:21:58 +0100

    script to collect all the objects before passing the next lvl

--- Assets/CommunScripts/LevelManager.cs
+++ Assets/CommunScripts/LevelManager.cs
@@ -1,11 +1,14 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
-
 
 public class LevelManager : MonoBehaviour
 {
     public static LevelManager Instance;
     
+    // ✅ AÑADE estas variables
+    private int totalCollectibles = 0;
+    private int collectedCount = 0;
+    private bool allCollectiblesCollected = false;
     
     void Awake()
     {
@@ -13,6 +16,8 @@ public class LevelManager : MonoBehaviour
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
+
+            SceneManager.sceneLoaded += OnSceneLoaded;
         }
         else
         {
@@ -20,10 +25,53 @@ public class LevelManager : MonoBehaviour
         }
     }
     
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // Reiniciar contadores en nuevo nivel
+        totalCollectibles = 0;
+        collectedCount = 0;
+        allCollectiblesCollected = false;
+        CountCollectibles();
+    }
+    
+
+    void Start()
+    {
+        CountCollectibles();
+    }
+    
+    void CountCollectibles()
+    {
+        // Contar todos los objetos Coleccionable en la escena
+        Coleccionable[] collectibles = FindObjectsOfType<Coleccionable>();
+        totalCollectibles = collectibles.Length;
+        Debug.Log($"Coleccionables en nivel: {totalCollectibles}");
+    }
+    
+
+    public void CollectItem()
+    {
+        collectedCount++;
+        Debug.Log($"Coleccionados: {collectedCount}/{totalCollectibles}");
+        
+        // Verificar si se recolectaron todos
+        if (collectedCount >= totalCollectibles && totalCollectibles > 0)
+        {
+            allCollectiblesCollected = true;
+            Debug.Log("¡Todos los coleccionables recolectados! Meta activada.");
+        }
+    }
+    
+
+    public bool CanPassToNextLevel()
+    {
+        return allCollectiblesCollected || totalCollectibles == 0;
+    }
+    
     public void LoadNextLevel()
     {
-       
-    int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         int nextSceneIndex = currentSceneIndex + 1;
         
         if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
@@ -32,7 +80,12 @@ public class LevelManager : MonoBehaviour
         }
         else
         {
-            SceneManager.LoadScene(0); // Volver al men�
+            SceneManager.LoadScene(0);
         }
+    }
+    
+    void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 }
